93b19c9474d8bc9b96c811b7b5cb728f3a9e4df0
ThanhNgo <thanhngo26@hotmail.com>
2020-06-25 15:42:13 -0700

    remove hide to show questions

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const startButton = document.getElementById("start-btn");
 startButton.addEventListener("click", startGame);
 
+const questionContainerElement = document.getElementById("question-container");
+
 function startGame(){
     console.log("Started")
     startButton.classList.add("hide");
+
+    questionContainerElement.classList.remove("hide");
 }
 
 function setNextQuestion(){
